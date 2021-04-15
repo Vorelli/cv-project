5e1c06110bb8b9e4a7e2ae25dd2c85de0da85dd5
Natale Toscano <kickalliance195@gmail.com>
2021-04-15 03:29:47 -0400

    Good spot before adding the generate/print CV functionality

--- src/data/experienceFields.js
+++ src/data/experienceFields.js
@@ -5,7 +5,7 @@ export default [
   { class: 'startYear', label: 'Start Year:' },
   { class: 'endMonth', label: 'End Month (N/A if presently employed):' },
   { class: 'endYear', label: 'End Year (N/A if presently employed):' },
-  { class: 'oneLineSummary', label: 'One line sumamry:' },
+  { class: 'oneLineSummary', label: 'One line sumary:' },
   {
     class: 'responsibilities',
     label: 'Responsibilities/Achievements (3-5 reccomended):',
